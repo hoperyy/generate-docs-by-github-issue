a63de7aab6796f215e4c251ac1a11c15d477d8c9
hoperyy <antfootmail@gmail.com>
2018-05-29 21:58:50 +0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const request = require('request');
 const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
+const co = require('co');
 
 function writeFileSync(filePath, content) {
     fse.ensureFileSync(filePath);
@@ -21,21 +22,20 @@ function run({ targetDir, username, repo, preWriting }) {
         return;
     }
 
-    const options = {
-        url: `https://api.github.com/repos/${username}/${repo}/issues`,
-        headers: {
-            'User-Agent': 'request'
+    co(function*() {
+        let allIssues = [];
+        let pageNum = 1;
+        let onePageIssues = yield _request(`?page=1`);
+        while (onePageIssues.length) {
+            allIssues = allIssues.concat(onePageIssues);
+            pageNum++;
+            onePageIssues = yield _request(`?page=${pageNum}`);
         }
-    };
 
-    _request()
-        .then(sortedIssues => {
-            // fse.removeSync(this.targetDir);
-            sortedIssues.forEach(createMarkdownByIssueItem);
-        })
-        .catch(err => {
-            throw Error(err);
-        });
+        const sortedIssues = sortByTitle(allIssues);
+
+        sortedIssues.forEach(createMarkdownByIssueItem);
+    });
 
     function createMarkdownByIssueItem(issueItem) {
         if (preWriting) {
@@ -52,26 +52,30 @@ function run({ targetDir, username, repo, preWriting }) {
         writeFileSync(targetMarkdown, content);
     }
 
-    function _request() {
-        return new Promise((resolve, reject) => {
-            request(options, (error, response, body) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
+    function _request(params) {
+        return done => {
+            request({
+                url: `https://api.github.com/repos/${username}/${repo}/issues${params}`,
+                    headers: {
+                        'User-Agent': 'request'
+                    }
+                }, (error, response, body) => {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
 
-                if (!response || response.statusCode !== 200) {
-                    reject('response code not 200');
-                    return;
-                }
+                    if (!response || response.statusCode !== 200) {
+                        reject('response code not 200');
+                        return;
+                    }
 
-                // sort by title
-                const sortedIssues = sortByTitle(JSON.parse(body));
+                    // sort by title
+                    // const sortedIssues = sortByTitle(JSON.parse(body));
 
-                resolve(sortedIssues);
-                // console.log('body:', sortedIssues); // Print the HTML for the Google homepage.
-            });
-        });
+                    done(null, JSON.parse(body));
+                });
+        }
     }
 
     function sortByTitle(issues) {
